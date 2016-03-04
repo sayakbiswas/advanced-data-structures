bb8725a61cabb547deb2d13be58b4b3bb14f1a4e
Sayak Biswas <sayak90@gmail.com>
2016-03-04 01:05:52 -0500

    Changelog: 1. Created class for the RedBlackTree data structure. 2. Added the node structure.

--- src/edu/ufl/cop5536spring2016/RedBlackTreeEventCounter.java
+++ src/edu/ufl/cop5536spring2016/RedBlackTreeEventCounter.java
@@ -3,6 +3,6 @@ package edu.ufl.cop5536spring2016;
 public class RedBlackTreeEventCounter {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
     }
 }
