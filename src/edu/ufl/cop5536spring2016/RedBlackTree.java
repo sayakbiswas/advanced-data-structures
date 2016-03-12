424501f0bc18a9f1091102dc00ac7f80e494f34e
sayakbiswas <sayak90@gmail.com>
2016-03-11 19:40:58 -0500

    Changelog: 1. Added comments. 2. Changed color property type to enum.

--- src/edu/ufl/cop5536spring2016/RedBlackTree.java
+++ src/edu/ufl/cop5536spring2016/RedBlackTree.java
@@ -24,20 +24,72 @@ public class RedBlackTree {
      * 4. Color
      */
     private class RedBlackTreeNode {
-        private int ID;
-        private int count;
-        private RedBlackTreeNode leftNode = null, rightNode = null;
-        private boolean color;
 
-        // The constructor to initialize a node
-        public RedBlackTreeNode(int ID, int count, boolean color) {
+        /**
+         * Holds the ID of the node
+         */
+        private int ID;
+
+        /**
+         * Holds the count value
+         */
+        private int count;
+
+        /**
+         * Hold the left and right children nodes
+         */
+        private RedBlackTreeNode leftNode = null, rightNode = null;
+
+        /**
+         * Holds the color of the node
+         */
+        private NodeColor nodeColor;
+
+        /**
+         * The constructor to initialize a node
+         * @param ID The key of the node
+         * @param count The data value of the node
+         */
+        public RedBlackTreeNode(int ID, int count) {
             this.ID = ID;
             this.count = count;
-            this.color = color;
+            this.nodeColor = NodeColor.RED; //Initialize nodes with RED nodeColor
+        }
+
+        /**
+         * Returns the color of the Red Black tree node.
+         * @return NodeColor
+         */
+        public NodeColor getNodeColor() {
+            return this.nodeColor;
+        }
+
+        /**
+         * Set the color of the Red Black tree node.
+         * @param nodeColor The color of the node
+         */
+        public void setNodeColor(NodeColor nodeColor) {
+            this.nodeColor = nodeColor;
         }
     }
 
-    // The red-black color notations
-    private static final boolean RED = false;
-    private static final boolean BLACK = true;
+    /**
+     * Holds the root node of the tree.
+     */
+    private RedBlackTreeNode rootNode = null;
+
+    // The red-black Color notations
+    private enum NodeColor {
+        RED,
+        BLACK
+    }
+
+    /**
+     * Inserts a new node in the Red Black tree.
+     * @param ID The key of the node to be inserted into the tree.
+     * @param count The data value of the node to be inserted into the tree.
+     */
+    public void insertNode(int ID, int count) {
+
+    }
 }
