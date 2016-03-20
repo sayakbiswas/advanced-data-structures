88aace4ac84bc2d9f3326f3c8220eaaff3b865e9
Sayak Biswas <sayak90@gmail.com>
2016-03-20 17:00:04 -0400

    Changelog: 1. Removed Event.java. Replaced usages of the class with RedBlackTree.RedBlackTreeNode class. 2. Performance enhancements. Replaced Scanner with BufferedReader.

--- src/bbst.java
+++ src/bbst.java
@@ -1,7 +1,7 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * Created by Sayak Biswas on 3/16/2016.
@@ -14,33 +14,40 @@ public class bbst {
             System.out.println("Proper usage: java bbst test_100.txt < commands.txt > out_100.txt");
             System.exit(0);
         } else {
-            ArrayList<Event> eventArrayList = null;
+            ArrayList<RedBlackTree.RedBlackTreeNode> eventArrayList = null;
             RedBlackTreeEventCounter redBlackTreeEventCounter = null;
-            Scanner fileReader = null;
+            BufferedReader fileReader = null;
             try {
-                fileReader = new Scanner(new FileInputStream(args[0]));
+                fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
                 int lineCount = 0;
-                eventArrayList = new ArrayList<>();
-                while (fileReader.hasNext()) {
-                    String scannedLine = fileReader.nextLine();
-                    Scanner scannedToken = new Scanner(scannedLine);
+                StringTokenizer tokenizer;
+                String scannedLine;
+                while((scannedLine = fileReader.readLine()) != null) {
+                    tokenizer = new StringTokenizer(scannedLine);
                     if (lineCount > 0) {
-                        while (scannedToken.hasNext()) {
-                            int ID = scannedToken.nextInt();
-                            int count = scannedToken.nextInt();
-                            Event event = new Event(ID, count);
-                            eventArrayList.add(event);
-                        }
-                        scannedToken.close();
+                        int ID = Integer.parseInt(tokenizer.nextToken());
+                        int count = Integer.parseInt(tokenizer.nextToken());
+                        RedBlackTree.RedBlackTreeNode event = new RedBlackTree.RedBlackTreeNode(ID,count);
+                        eventArrayList.add(event);
+                    } else {
+                        int size = Integer.parseInt(tokenizer.nextToken());
+                        eventArrayList = new ArrayList<>(size);
                     }
                     lineCount++;
                 }
             } catch (FileNotFoundException fileNotFoundException) {
                 System.out.println("File " + args[0] + " not found!");
                 fileNotFoundException.printStackTrace();
-            } finally {
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            finally {
                 if(fileReader != null) {
-                    fileReader.close();
+                    try {
+                        fileReader.close();
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
                 }
             }
             if(eventArrayList != null) {
@@ -72,12 +79,14 @@ public class bbst {
                             System.out.println(redBlackTreeEventCounter.inRange(ID1, ID2));
                         } else if("next".equalsIgnoreCase(command)) {
                             int ID = stringScanner.nextInt();
-                            Event nextEvent = redBlackTreeEventCounter.next(ID);
-                            System.out.println(nextEvent.getID() + " " + nextEvent.getCount());
+                            RedBlackTree.RedBlackTreeNode nextEvent = redBlackTreeEventCounter.next(ID);
+                            System.out.println(((nextEvent != null) ? nextEvent.getID() : 0) + " "
+                                    + ((nextEvent != null) ? nextEvent.getCount() : 0));
                         } else if("previous".equalsIgnoreCase(command)) {
                             int ID = stringScanner.nextInt();
-                            Event previousEvent = redBlackTreeEventCounter.previous(ID);
-                            System.out.println(previousEvent.getID() + " " + previousEvent.getCount());
+                            RedBlackTree.RedBlackTreeNode previousEvent = redBlackTreeEventCounter.previous(ID);
+                            System.out.println(((previousEvent != null) ? previousEvent.getID() : 0) + " "
+                                    + ((previousEvent != null) ? previousEvent.getCount() : 0));
                         } else if("quit".equalsIgnoreCase(command)) {
                             break;
                         } else {
